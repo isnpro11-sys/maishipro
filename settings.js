cb2ae6769755a83b23d572909964f50e0670db11
isnpro11-sys <isnpro11@gmail.com>
2026-01-17 01:14:40 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,60 @@
+// settings.js
+
+const productData = [
+    {
+        name: "Sewa Bot Jaga Grub",
+        status: "close", // Gunakan 'open' atau 'close'.
+        variants: [
+            { name: "1 hari", price: 1000 },
+            { name: "2 hari", price: 2000 },
+            { name: "3 hari", price: 3000 },
+            { name: "4 hari", price: 4000 },
+            { name: "Permanen", price: 5000 }
+        ]
+    },
+    {
+        name: "Script Bot",
+        status: "open",
+        variants: [
+            { name: "Jaga group", price: 15000 },
+            { name: "store", price: 10000 }
+        ]
+    },
+    {
+        name: "Panel Bot",
+        status: "open",
+        variants: [
+            { name: "Ram 5gb", price: 3000 },
+            { name: "Ram 10gb", price: 5000 },
+            { name: "Ram Unli", price: 7000 }
+        ]
+    },
+    {
+        name: "Jasa Buat Website",
+        status: "open", 
+        variants: [
+            { name: "Web Payment", price: 5000 },
+            { name: "Web Jualan", price: 10000 }
+        ]
+    },
+    {
+        name: "Nokos Whatsapp",
+        status: "open", 
+        variants: [
+            { name: "Nokos Indonesia", price: 5000 },
+            { name: "Nokos Kolombia", price: 7000 },
+            { name: "Nokos Filipina", price: 7000 },
+            { name: "Nokos lainnya tanya pm", price: 0 }
+        ]
+    },
+    {
+        name: "Jasa Edit Preset",
+        status: "open", 
+        variants: [
+            { name: "Prst mudah", price: 2000 },
+            { name: "Prst lumayan", price: 4000 },
+            { name: "Prst susah", price: 6000 },
+            { name: "Prst susah banget", price: 8000 },
+        ]
+    }
+];
